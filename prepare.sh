78be756a6800ac871dfdd9c8c3015b6700c2fac0
Patrick Pfenning <patrick.pfenning.92@gmail.com>
2023-06-20 21:13:52 +0200

    add prepare script

---
+++ prepare.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+echo "UPDATING SYSTEM"
+sudo apt update -qqq && sudo apt -qqq -o Dpkg::Use-Pty=0 upgrade -y && sudo apt -qqq -o Dpkg::Use-Pty=0 dist-upgrade -y
+sudo apt -qqq -o Dpkg::Use-Pty=0 install git software-properties-common python3 python-is-python3 python3-pip -y
+
+echo "INSTALL ANSIBLE"
+pip --qiet install --no-warn-script-location --user ansible
+
