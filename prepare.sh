ebab927f5acd3ffca645fa3395f1c7a43df5ac5b
Patrick Pfenning 2 <patrick.pfenning.92@gmail.com>
2023-12-08 13:19:44 +0100

    WIP

--- prepare.sh
+++ prepare.sh
@@ -7,5 +7,5 @@ sudo apt update -qqq && sudo apt -qqq -o Dpkg::Use-Pty=0 upgrade -y && sudo apt 
 sudo apt -qqq -o Dpkg::Use-Pty=0 install git software-properties-common python3 python-is-python3 python3-pip -y
 
 echo "INSTALL ANSIBLE"
-sudo -H pip --quiet install --no-warn-script-location  ansible
+sudo -H pip --quiet install --no-warn-script-location  ansible ansible-lint
 
